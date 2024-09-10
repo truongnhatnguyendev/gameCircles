ecd500978a9af01915cc85f4453549e78f4a614f
truongnhatnguyen <truongnhatnguyen9999@gmail.com>
2024-09-10 09:19:13 +0700

    tối ưu performance

--- src/Game/GameCircle/index.tsx
+++ src/Game/GameCircle/index.tsx
@@ -1,4 +1,10 @@
-import React, { useState, useEffect, useCallback, useRef } from "react";
+import React, {
+  useState,
+  useEffect,
+  useCallback,
+  useRef,
+  useMemo,
+} from "react";
 import "./game.css";
 
 interface Circle {
@@ -20,8 +26,6 @@ export function GameCircle() {
   const [time, setTime] = useState<number>(0.0);
   const [timerRunning, setTimerRunning] = useState(false);
   const [gameStarted, setGameStarted] = useState(false);
-
-  // const [clickedIds, setClickedIds] = useState<number[]>([]);
 
   const [gameStatus, setGameStatus] = useState<string>(StatusEnum.default);
 
@@ -50,31 +54,14 @@ export function GameCircle() {
   }, [points]);
 
   const clickedIds = useRef<number[]>([]);
-  const handleCircleClick = (id: number) => {
-    clickedIds.current.push(id);
-    const temp = [...circles].filter(
-      (item) => !clickedIds.current.includes(item.id)
-    );
-    const isSmallerId = temp.every((item) => item.id > id);
-    if (isSmallerId && timerRunning === true) {
-      setCircles((prevCircles) =>
-        prevCircles.map((circle) =>
-          circle.id === id
-            ? { ...circle, backgroundColor: "red", color: "white" }
-            : circle
-        )
+  const handleCircleClick = useCallback(
+    (id: number) => {
+      clickedIds.current.push(id);
+      const temp = [...circles].filter(
+        (item) => !clickedIds.current.includes(item.id)
       );
-      setTimeout(() => {
-        setCircles((prevCircles) =>
-          prevCircles.filter((circle) => circle.id !== id)
-        );
-        if (id === points) {
-          setGameStatus(StatusEnum.WIN);
-          setTimerRunning(false);
-        }
-      }, 1000);
-    } else {
-      timerRunning &&
+      const isSmallerId = temp.every((item) => item.id > id);
+      if (isSmallerId && timerRunning === true) {
         setCircles((prevCircles) =>
           prevCircles.map((circle) =>
             circle.id === id
@@ -82,45 +69,73 @@ export function GameCircle() {
               : circle
           )
         );
-      setTimerRunning(false);
-      setGameStatus(StatusEnum.LOSE);
-    }
-  };
+        setTimeout(() => {
+          setCircles((prevCircles) =>
+            prevCircles.filter((circle) => circle.id !== id)
+          );
+          if (id === points) {
+            setGameStatus(StatusEnum.WIN);
+            setTimerRunning(false);
+          }
+        }, 1000);
+      } else {
+        timerRunning &&
+          setCircles((prevCircles) =>
+            prevCircles.map((circle) =>
+              circle.id === id
+                ? { ...circle, backgroundColor: "red", color: "white" }
+                : circle
+            )
+          );
+        setTimerRunning(false);
+        setGameStatus(StatusEnum.LOSE);
+      }
+    },
+    [circles, points, timerRunning]
+  );
 
-  const handlePlay = () => {
+  const handlePlay = useCallback(() => {
     if (points > 0) {
       setGameStarted(true);
       startGame();
     }
-  };
+  }, [points, startGame]);
 
-  const handleRestart = () => {
+  const handleRestart = useCallback(() => {
     setTime(0.0);
     setCircles([]);
     startGame();
     clickedIds.current = [];
     setGameStatus(StatusEnum.default);
-  };
+  }, [startGame]);
 
   const handleChangePoints = (points: number) => {
     setPoints(points);
   };
 
+  const memoizedCircles = useMemo(() => {
+    return circles.map((circle) => (
+      <div
+        key={circle.id}
+        className="circle"
+        style={{
+          top: `${circle.position.top}%`,
+          left: `${circle.position.left}%`,
+          backgroundColor: circle.backgroundColor,
+          color: circle.color,
+          zIndex: points - circle.id,
+        }}
+        onClick={() => handleCircleClick(circle.id)}
+      >
+        {circle.id}
+      </div>
+    ));
+  }, [circles, points]);
+
   return (
     <div className="game-container">
       <div style={{ margin: "0 auto", width: "50vw" }}>
-        <h3
-          style={{
-            color:
-              gameStatus === StatusEnum.WIN
-                ? "green"
-                : gameStatus === StatusEnum.LOSE
-                ? "red"
-                : "",
-          }}
-        >
-          {gameStatus}
-        </h3>
+        {<GameStatus gameStatus={gameStatus} />}
         <div className="scoreboard">
           <div>
             Points:
@@ -130,30 +145,32 @@ export function GameCircle() {
               onChange={(e) => handleChangePoints(parseInt(e.target.value))}
             />
           </div>
-          <div>Time: {time.toFixed(1) + "s"}</div>
+          {<Timer time={time} />}
           <button onClick={gameStarted ? handleRestart : handlePlay}>
             {gameStarted ? "Restart" : "Play"}
           </button>
         </div>
       </div>
-      <div className="game-board">
-        {circles.map((circle) => (
-          <div
-            key={circle.id}
-            className="circle"
-            style={{
-              top: `${circle.position.top}%`,
-              left: `${circle.position.left}%`,
-              backgroundColor: circle.backgroundColor,
-              color: circle.color,
-              zIndex: points! - circle.id,
-            }}
-            onClick={() => handleCircleClick(circle.id)}
-          >
-            {circle.id}
-          </div>
-        ))}
-      </div>
+      <div className="game-board">{memoizedCircles}</div>
     </div>
   );
 }
+
+const GameStatus = React.memo(({ gameStatus }: { gameStatus: string }) => (
+  <h3
+    style={{
+      color:
+        gameStatus === StatusEnum.WIN
+          ? "green"
+          : gameStatus === StatusEnum.LOSE
+          ? "red"
+          : "",
+    }}
+  >
+    {gameStatus}
+  </h3>
+));
+
+const Timer = React.memo(({ time }: { time: number }) => (
+  <div>Time: {time.toFixed(1)}s</div>
+));
